15746b2ff53c9caee58065e02b8a61e558a7382a
MorksMQ <bertincamacho2@gmail.com>
2024-08-21 11:49:49 -0600

    0.07.1
    solo se arreglo el añadir productos

--- añadirproductos.php
+++ añadirproductos.php
@@ -3,10 +3,11 @@ session_start();
 
 // Verificar si el usuario está conectado y si es administrador
 if (!isset($_SESSION['usuario_id']) || $_SESSION['rol'] !== 'admin') {
-    
+    // Si no está conectado o no es administrador, redirigir a logout.php
     header("Location: logout.php");
     exit();
 }
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -15,6 +16,6 @@ if (!isset($_SESSION['usuario_id']) || $_SESSION['rol'] !== 'admin') {
     <title>Añadir productos</title>
 </head>
 <body>
-    
+    <p>Eres admin</p>
 </body>
 </html>
